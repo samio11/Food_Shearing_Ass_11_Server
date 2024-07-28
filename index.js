ea59a8a0238f3e490d097877a1f0a833f30e6adc
Samio Hasan <138763106+samio11@users.noreply.github.com>
2024-07-28 14:10:44 +0600

    Jwt and cokkie done for user login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +15,26 @@ const corsConfig = {
 
 app.use(cors(corsConfig));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized User'});
+  }
+  if(token)
+  {
+    jwt.verify(token, process.env.Access_Token, (err, decodedToken) => {
+      if (err) {
+        return res.status(403).send({message: 'Invalid Token'});
+      }
+      req.user = decodedToken;
+      next();
+    });
+  }
+ 
+}
 
 app.get('/',(req,res)=>{
     res.send("YOO! Server IS Running")
@@ -34,6 +56,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.Access_Token,{expiresIn: '7d'})
+      res
+      .cookie('token',token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production'  ? 'none' : 'strict'
+      })
+      .send({success: true, message: 'JWT token sent successfully'})
+    })
+
+    app.get('/logout', async(req,res)=>{
+      res.clearCookie('token',{
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production'  ? 'none' : 'strict',
+        maxAge: 0
+      })
+      .send({success: true, message: 'Cookie Clear Done'})
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
